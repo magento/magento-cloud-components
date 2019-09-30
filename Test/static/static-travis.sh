930ae6a71dc8ddd4584a3d7a3d3935efe2419420
Oleksandr Shmyheliuk <oshmyheliuk@magento.com>
2019-09-30 09:27:53 -0500

    MAGECLOUD-3967: Add static and unit tests to magento-cloud-components

--- Test/static/static-travis.sh
+++ Test/static/static-travis.sh
@@ -6,7 +6,16 @@
 set -e
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
 
+function log() {
+    echo "Running {$1} tests:"
+}
+
+log "phpstan"
 ./vendor/bin/phpstan analyse -c Test/static/phpstan.neon
+log "phpcs"
 ./vendor/bin/phpcs ./ --standard=Test/static/phpcs-ruleset.xml -p -n
+log "phpmd"
 ./vendor/bin/phpmd Console xml Test/static/phpmd-ruleset.xml
+log "phpunit"
 ./vendor/bin/phpunit --configuration Test/Unit
+
